2bc47d16e12361b9591ec985a29927c76aed073f
Sheena Chawla <sheena2342@gmail.com>
2014-06-17 16:30:42 +0530

    with constraints

--- utils/sqlite2rec.c
+++ utils/sqlite2rec.c
@@ -45,7 +45,7 @@ int main(int argc, char** args)
         s2r->record = rec_record_new();     //initialising a new record i.e. for mysql it is a new row
         s2r->recfield = rec_field_new ("%rec", args[2]);       //initialising the record descriptor
         rec_mset_append (rec_record_mset (s2r->record), MSET_FIELD, (void *) s2r->recfield, MSET_ANY);
-        rec_rset_set_descriptor (s2r->rset, s2r->record); 
+         
 
 
     // Create an int variable for storing the return code for each call
@@ -83,7 +83,138 @@ int main(int argc, char** args)
    
     // Read the number of rows fetched
         int cols = sqlite3_column_count(stmt);
-           
+        int col;
+         int j=0;
+  int i=0;
+  #define TYPE_VALUE_SIZE 250
+  char type_value[TYPE_VALUE_SIZE];
+  char *fnull[100];
+  char *funi[100];
+  char *fpri[100];  
+  char *fauto[100];
+  int count=0;
+  int count1=0;
+  int count2=0;
+  int count3=0;
+  char *f[100];     //array to store all field names
+  int x=0;
+    
+for(col=0;col<cols;col++)   //loop until field name is fetched 
+{ 
+    
+    type_value[0] = 0;
+    if(j<=cols)
+    f[j]=strdup(sqlite3_column_name(stmt,col));   //all field names are copied into a variable f[] one by one.
+    j++;  
+    int pNotNull;
+    int pPrimaryKey; 
+    int pAutoinc;
+    const char  *pzDataType, *pzCollSeq;
+    sqlite3_table_column_metadata(handle,NULL, args[2], sqlite3_column_name(stmt,col),  &pzDataType, &pzCollSeq, &pNotNull, &pPrimaryKey,&pAutoinc );
+
+    
+
+
+     if(pAutoinc==1)         //checking if field has an auto_increment type 
+     {
+          fauto[count3]=strdup(sqlite3_column_name(stmt,col));    //storing the fields which are auto_incremented in an array.
+          count3++;  
+          
+     }
+     if(pNotNull==1)               //checking if field is NOT NULL i.e. mandatory
+     {    
+     
+          fnull[count]=strdup(sqlite3_column_name(stmt,col));       //storing the fields which are mandatory in an array.
+          count++;
+          
+     }
+
+
+    if(pPrimaryKey==1)           //checking if the field is primary 
+     {
+            fpri[count2]=strdup(sqlite3_column_name(stmt,col));
+            count2++;      
+            
+      }
+
+     /*checking the type of field-if it is real*/
+      if(strcmp(pzDataType,"FLOAT")==0)
+      {
+         snprintf (type_value, TYPE_VALUE_SIZE,"%s real", sqlite3_column_name(stmt,col));
+        
+      }
+      
+      /*checking the type of field-if it is int*/
+      if(strcmp(pzDataType,"INTEGER")==0)
+      {
+        snprintf (type_value, TYPE_VALUE_SIZE,"%s int", sqlite3_column_name(stmt,col));
+       
+      }
+
+
+       /*checking the type of field-if it is date*/
+      /*if(MYSQL_TYPE_DATE == field->type)
+      {
+        snprintf (type_value, TYPE_VALUE_SIZE,"%s date", field->name);
+
+      }*/
+
+  if (type_value[0] != 0)
+        {
+          /* Insert a type field for this field.  */
+          s2r->recfield = rec_field_new ("%type", type_value);
+          rec_mset_append (rec_record_mset (s2r->record), MSET_FIELD, (void *) s2r->recfield, MSET_ANY);
+        }
+  
+  
+
+}  
+/*copying the fields in proper format(if more than 1 field which are mandatory are there in record set)*/
+if(count!=0)
+{
+char s[100]="";
+for(x=0;x<count;x++)
+{
+       strcat(s,fnull[x]);
+       strcat(s," ");
+                     
+}
+      s2r->recfield = rec_field_new ("%mandatory",s);
+      rec_mset_append (rec_record_mset (s2r->record), MSET_FIELD, (void *) s2r->recfield, MSET_ANY);
+}
+
+
+
+/*copying the fields in proper format(if more than 1 field which are primary keys(composite primary key in mysql) are there in record set)*/
+if(count2!=0)
+{
+  char s[100]="";
+for(x=0;x<count2;x++)
+{
+       strcat(s,fpri[x]);
+       strcat(s," ");        
+                        
+}
+
+      s2r->recfield = rec_field_new ("%key",s);
+      rec_mset_append (rec_record_mset (s2r->record), MSET_FIELD, (void *) s2r->recfield, MSET_ANY);
+}
+
+/*copying the fields in proper format(if more than 1 field which are auto_incremented are there in record set)*/
+if(count3!=0)
+{
+  char s[100]="";
+for(x=0;x<count3;x++)
+{
+       strcat(s,fauto[x]);
+       strcat(s," ");
+                     
+}
+      s2r->recfield = rec_field_new ("\%auto",s);
+      rec_mset_append (rec_record_mset (s2r->record), MSET_FIELD, (void *) s2r->recfield, MSET_ANY);
+}
+
+        rec_rset_set_descriptor (s2r->rset, s2r->record);   
         while(1)
         {
         // fetch a row's status
